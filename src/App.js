46adf9e874ae1aeb159f8276c10ccbc5f8dbeb3c
Laura <Laura.Lively@gmail.com>
2025-02-01 17:11:58 -0700

    adding some CSS and a login page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Homepage from './pages/Home';
 import CharacterCreatePage from './pages/CreateCharacter';
 import SpellsPage from './pages/Spells';
+import Login from './pages/Login';
 import NoMatch from './pages/error404';
 
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
+import './components/Header/header.css';
 
 function App() {
   return (
@@ -20,7 +22,7 @@ function App() {
           <Route index element={<Homepage />}></Route>
           <Route path="/CharacterCreate" element={<CharacterCreatePage />}></Route>
           <Route path="/Spells" element={<SpellsPage />}></Route>
-
+          <Route path="/Login" element={<Login />}></Route>
           <Route path="*" element={<NoMatch />}></Route>
         </Route>
       </Routes>
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+
+
+function Login () {
+
+    return (
+        <>
+            <p>Login Page</p>
+        </>
+    )
+}
+
+export default Login;
