070e58173f441317a5a4fb4a612d643a9decf47b
Easton Shaum <eastonshaum@gmail.com>
2025-02-06 16:09:19 -0700

    Adding in more stuff for loading API calls...doesnt work yet lol
    Co-authored-by: Hai T Le <hai.t.le16@gmail.com>

--- src/pages/Spells.js
+++ src/pages/Spells.js
@@ -5,18 +5,23 @@ import SpellCard from "../components/Spells/spellcard";
 import "../components/Spells/styles.css"
 
 function SpellsPage () {
+    //Loading stuff doesnt work but its not breaking it so I am pushing lol
+    const [isLoading, setIsLoading] = useState(true);
     const [spells, setSpells] = useState([]);
 
-    loadingText();
-
     useEffect(() => {
-        getAllSpells().then(setSpells);
+      setIsLoading(true);
+      console.log("during", isLoading)
+        getAllSpells()
+          .then(setSpells)
+          .then(setIsLoading(false));
     }, []);
+    console.log("after", isLoading)
 
     return (
         <>
             <p>List of all spells:</p>
-            <p id="loadingText">Loading...</p>
+            {isLoading ? <p>Loading...</p> : null}
             <ul id="spellList" className="spell-list">
               {spells.map((spell) => (
                 <SpellCard key={spell.index} spell={spell} />
@@ -24,10 +29,6 @@ function SpellsPage () {
             </ul>
         </>
     )
-}
-
-function loadingText() {
-    
 }
 
 export default SpellsPage;
