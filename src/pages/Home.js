e19aad5a803b3617b3cab6313b4463210c18d4b2
Laura Lively <Laura.Lively@gmail.com>
2025-02-09 15:38:20 -0700

    Adding logo and changing footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { Button } from "react-bootstrap";
-
+import logo from "../assets/logos/Design-and-Destroy-logo.png"
 import DiceContainer from "../calc/diceRoller";
 
 
@@ -7,10 +7,18 @@ function Homepage () {
 
     return (
         <>
-            <p>Hello World!</p>
-            <Button href="#/Spells">Spells</Button>
+        <div class="text-center">
+            <h2>Welcome to</h2>
+            <img src={logo} />
+        </div>
+        <div id="btn-container" class="d-flex justify-content-evenly grid gap-3 mt-4 mb-4">
+            <Button type="button" class="btn-paper btn-secondary p-2 g-col-3" href="#/Spells">Create a Character</Button>
+            <Button type="button" class="btn btn-primary p-2 g-col-3" href="#/Spells">Game Rules</Button>
+            <Button type="button" class="btn btn-primary p-2 g-col-3" href="#/Spells">Classes</Button>
+            <Button type="button" class="btn btn-primary p-2 g-col-3" href="#/Spells">Spells</Button>
+        </div>
 
-            <DiceContainer></DiceContainer>
+            {/* <DiceContainer></DiceContainer> */}
         </>
     )
 }
