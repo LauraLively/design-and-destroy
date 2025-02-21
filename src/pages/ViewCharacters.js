0c71b0186b8aca5e4fed5141547f653507a83115
Easton Shaum <eastonshaum@gmail.com>
2025-02-20 20:24:23 -0700

    view characters page and mock data. Also undoing the footer thing Laura was mad about lol

--- src/pages/ViewCharacters.js
+++ src/pages/ViewCharacters.js
@@ -1,11 +1,50 @@
+import React,{useState,useEffect} from 'react';
 
+import CharacterData from "../database/testData/chars.json"
 
 
 function CharacterViewPage() {
+    // To use laterrrrr
+    // const [data,setData]=useState([]);
+    // const getData= async ()=>{
+    //     await fetch('chars.json'
+    //     ,{
+    //         headers : { 
+    //         'Content-Type': 'application/json',
+    //         'Accept': 'application/json'
+    //        }
+    //     }
+    //     )
+    //       .then(function(response){
+    //         console.log("response", response)
+    //         return response.json();
+    //       })
+    //       .then(function(myJson) {
+    //         console.log("myjson", myJson.userCharacters);
+    //         setData(myJson.userCharacters[0])
+    //       });
+    // }
+    // useEffect(()=>{
+    //     getData()
+    // },[])
+
+    console.log("CharacterData", CharacterData)
+
+    const CharData=CharacterData.userCharacters.map((character) =>{
+        return(
+            <div>
+                <p>{character.id}</p>
+                <p>{character.Name}</p>
+            </div>
+        )
+    })
     return(
-        <div>
-            
-        </div>
+        <>
+            <div>
+                <h1>Select a Character below to view more about it</h1>
+                {CharData}
+            </div>
+        </>
     )
 
 }
